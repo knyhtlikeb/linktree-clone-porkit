cffe3a77cd23f69cd72cc809acd6e647e8a1651c
knyhtlikeb <159398362+knyhtlikeb@users.noreply.github.com>
2024-03-04 22:24:15 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/App.test.js 	new file:   src/Icon.js 	modified:   src/index.css 	modified:   src/index.js 	new file:   src/porkit.jpg 	modified:   src/reportWebVitals.js 	modified:   src/setupTests.js 	new file:   tailwind.config.js "Updated: wat? bugs following bugs! anyone want knows how to fix it few lines of code then just fork it/ pork it that and make it as your own"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import profilePic from './porkit.jpg';
+import { FaGithub,
+         FaInstagram, 
+         FaTelegram, 
+         FaTwitter, 
+         FaYoutube } from 'react-icons/fa';
+         // eslint-disable-next-line 
+         import { createClient } from "@supabase/supabase-js";
 
 function App() {
+  
+  const links = [
+    {
+      url: 'https://www.youtube.com/',
+      title: 'YouTube',
+      icon: <FaYoutube />
+    },
+  {
+    url: 'https://www.twitter.com/',
+    title: 'Twitter',
+    icon: <FaTwitter />
+  },
+  {
+    url: 'https://www.telegram.com/',
+    title: 'Telegram',
+    icon: <FaTelegram />
+  },
+  {
+    url: 'https://www.instagram.com/',
+    title: 'Instagram',
+    icon: <FaInstagram />
+  },
+  {
+    url: 'https://www.github.com/',
+    title: 'GitHub',
+    icon: <FaGithub />
+  },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header container mx-auto">
+        <img src={profilePic} className="profile-pic w-20 h-20 mb-4 rounded-full" alt="logo" />
+        <ul className='flex flex-col w-full'>
+          {
+            links.map((link, index) => (
+              <li key={index} className='cursor-pointer m-2 py-3 px-10 border-black border-2 text-sm md:text-md text-slate-900'>
+                <a href={link.url} className='flex items-center' title={link.title} target="_blank" rel='nonreferrer '>
+                 {link.icon} <p className='ml-3'>{link.title}</p>
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
